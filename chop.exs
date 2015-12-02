414ff7437ee44628a484aaddc44fb2f917045040
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-12-02 22:29:58 +0200

    Make guess helper functions private

--- chop.exs
+++ chop.exs
@@ -7,24 +7,24 @@ defmodule Chop do
     guess(number, middle(last, first), last..first)
   end
 
-  def guess(number, guess, _) when number === guess do
+  defp guess(number, guess, _) when number === guess do
     print guess
     guess
   end
 
-  def guess(number, guess, _..last) when div(number, guess) > 0 do
+  defp guess(number, guess, _..last) when div(number, guess) > 0 do
     print guess
     guess(number, middle(guess, last), guess..last)
   end
 
-  def guess(number, guess, first.._) when div(number, guess) == 0 do
+  defp guess(number, guess, first.._) when div(number, guess) == 0 do
     print guess
     guess(number, middle(first, guess), first..guess)
   end
 
-  def middle(first, last) do
+  defp middle(first, last) do
     first + div(last - first, 2)
   end
 
-  def print(guess), do: IO.puts "Is it #{guess}?"
+  defp print(guess), do: IO.puts "Is it #{guess}?"
 end
