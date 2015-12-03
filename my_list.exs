6cc873868e1983a4ab6b7a5a1c9f2f4051d376ba
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-12-03 17:08:47 +0200

    Add ruduce function

--- my_list.exs
+++ my_list.exs
@@ -1,16 +1,21 @@
 defmodule MyList do
-  def len([]),             do: 0
-  def len([_head | tail]), do: 1 + len(tail)
+  def map([], _func),           do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
+
+  def reduce([], value, _func), do: value
+  def reduce([head | tail], value, func) do
+    reduce(tail, func.(head, value), func)
+  end
 
   def square(list), do: map(list, &(&1 * &1))
 
   def add_1(list), do: map(list, &(&1 + 1))
 
-  def map([], _func),           do: []
-  def map([head | tail], func), do: [func.(head) | map(tail, func)]
-
   def sum_no_acc([]),            do: 0
   def sum_no_acc([head | tail]), do: head + sum_no_acc(tail)
+
+  def len([]),             do: 0
+  def len([_head | tail]), do: 1 + len(tail)
 
   def sum(list), do: sum(list, 0)
 
