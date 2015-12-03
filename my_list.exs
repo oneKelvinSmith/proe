af7001574a966154e474afbf4de84598c56ddcaf
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-12-03 11:20:30 +0200

    Generalise to map function

--- my_list.exs
+++ my_list.exs
@@ -2,9 +2,10 @@ defmodule MyList do
   def len([]),             do: 0
   def len([_head | tail]), do: 1 + len(tail)
 
-  def square([]),            do: []
-  def square([head | tail]), do: [head * head | square(tail)]
+  def square(list), do: map(list, &(&1 * &1))
 
-  def add_1([]),            do: []
-  def add_1([head | tail]), do: [head + 1 | add_1(tail)]
+  def add_1(list), do: map(list, &(&1 + 1))
+
+  def map([], _func),           do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
 end
