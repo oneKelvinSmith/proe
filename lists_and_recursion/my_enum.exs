fa8e59eb066a3982a1d9ca841643d01b39cff4d1
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-03-29 15:24:59 +0200

    Some formatting of MyEnum module

--- lists_and_recursion/my_enum.exs
+++ lists_and_recursion/my_enum.exs
@@ -6,7 +6,7 @@ defmodule MyEnum do
   def each([], _func), do: :ok
 
   def all?([head | tail], func), do: func.(head) and all?(tail, func)
-  def all?([], _func), do: true
+  def all?([], _func),           do: true
 
   def filter([head | tail], func) do
     if func.(head) do
@@ -18,7 +18,7 @@ defmodule MyEnum do
   def filter([], _func), do: []
 
   def split(list, count) when count < 0, do: split(list, count(list) + count)
-  def split(list, count), do: do_split([], list, count)
+  def split(list, count),                do: do_split([], list, count)
   defp do_split(left, [head | tail], count) when count > 0 do
     do_split([head | left], tail, count - 1)
   end
@@ -26,10 +26,12 @@ defmodule MyEnum do
 
   def count(list), do: do_count(list, 0)
   defp do_count([_head | tail], count), do: do_count(tail, count + 1)
-  defp do_count([], count), do: count
+  defp do_count([], count),             do: count
 
   def reverse([head | tail]), do: do_reverse(tail, [head])
-  def reverse([]), do: []
-  defp do_reverse([head | tail], reversed), do: do_reverse(tail, [head | reversed])
+  def reverse([]),            do: []
+  defp do_reverse([head | tail], reversed) do
+    do_reverse(tail, [head | reversed])
+  end
   defp do_reverse([], reversed), do: reversed
 end
