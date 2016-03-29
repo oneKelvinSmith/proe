93ee1b767a91ff391703036458c007e111013b4d
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-03-29 14:54:52 +0200

    Implement Enum.split and Enum.reverse

--- lists_and_recursion/my_enum.exs
+++ lists_and_recursion/my_enum.exs
@@ -18,4 +18,21 @@ defmodule MyEnum do
     end
   end
   def filter([], _func), do: []
+
+  def split(list, count) do
+    do_split(list, {count, [], []})
+  end
+  defp do_split([head | tail], {count, left, right}) do
+    if count > 0 do
+      left = [head | left]
+    else
+      right = [head | right]
+    end
+    do_split(tail, {count - 1, left, right})
+  end
+  defp do_split([], {_, left, right}), do: {reverse(left), reverse(right)}
+
+  def reverse([head | tail]), do: do_reverse(tail, [head])
+  defp do_reverse([head | tail], reversed), do: do_reverse(tail, [head | reversed])
+  defp do_reverse([], reversed), do: reversed
 end
