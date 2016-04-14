895d00447bdbc517fca0e09e4d6356570ebd8403
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-04-14 09:28:43 +0100

    Add enum examples

---
+++ enum/stream.exs
@@ -0,0 +1,10 @@
+[1, 2, 3, 4]
+|> Stream.map(&(&1 * &1))
+|> Stream.map(&(&1 + 1))
+|> Stream.filter(fn x -> rem(x, 2) == 1 end)
+|> Enum.to_list
+|> IO.inspect
+
+File.stream!("/usr/share/dict/words")
+|> Enum.max_by(&String.length/1)
+|> IO.puts
