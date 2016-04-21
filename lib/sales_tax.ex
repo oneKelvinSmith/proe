3c6416d1fc09673fe20924561f0d47b64751d9ae
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-04-21 21:21:19 +0100

    Add sales tax exercise

---
+++ lib/sales_tax.ex
@@ -0,0 +1,20 @@
+defmodule SalesTax do
+  def calculate_totals(tax_rates, orders) do
+    for order <- orders do
+      case order do
+        [_, {:ship_to, :NC},  _] ->
+          Keyword.get(tax_rates, :NC)
+        [_, {:ship_to, :TX}, _] ->
+          Keyword.get(tax_rates, :TX)
+        otherwise -> 0
+      end
+      |> set_total_amount(order)
+    end
+  end
+
+  defp set_total_amount(rate, order) do
+    net = Keyword.get(order, :net_amount)
+    total = net * (1 + rate)
+    Keyword.put(order, :total_amount, total)
+  end
+end
