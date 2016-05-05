bdb621aefbfc1f2aa8d11a1c2e4f1e831e90d996
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-05-05 10:04:54 +0100

    Add function to center words

--- lib/words.ex
+++ lib/words.ex
@@ -9,4 +9,26 @@ defmodule Words do
     |> String.to_char_list
     |> Enum.sort
   end
+
+  def center([]), do: :ok
+  def center([head | tail] = words) when is_list(words) do
+    max = max_length words
+    Enum.each words, fn(word) ->
+      IO.puts justify(word, max)
+    end
+  end
+
+  defp justify(word, max) do
+    relative_length = div(max + String.length(word), 2)
+    String.rjust(word, relative_length)
+  end
+
+  defp max_length(words) do
+    Enum.reduce words, 0, fn
+      word, acc when byte_size(word) > acc ->
+        byte_size(word)
+      _word, acc ->
+        acc
+    end
+  end
 end
--- test/words_test.exs
+++ test/words_test.exs
@@ -1,6 +1,8 @@
 defmodule WordsTest do
   use ExUnit.Case
 
+  import ExUnit.CaptureIO
+
   test "anagram?/2 returns true when the given words are anagrams of each other" do
     assert Words.anagram?("dog", "god") == true
   end
@@ -11,5 +13,17 @@ defmodule WordsTest do
 
   test "anagram?/2 returns false when words of same lenght are not anagrams of each other" do
     assert Words.anagram?("inextricably", "unmanageable") == false
+  end
+
+  test "center/1 centers the words in a column as wide as the longest word" do
+    assert capture_io(fn ->
+      Words.center(["cat", "zebra", "elephant"])
+    end) == "  cat\n zebra\nelephant\n"
+  end
+
+  test "center/1 does returns and empty strin when the list is empty" do
+    assert capture_io(fn ->
+      Words.center([])
+    end) == ""
   end
 end
