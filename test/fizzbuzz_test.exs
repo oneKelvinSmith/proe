01f074c94bf107e216f969198d097a1812a7de54
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-05-25 21:52:35 +0100

    Implement FizzBuzz with cond

---
+++ test/fizzbuzz_test.exs
@@ -0,0 +1,27 @@
+defmodule FizzBuzzTest do
+  use ExUnit.Case
+
+  test 'upto/1 outputs "Fizz" for multiples of three' do
+    assert FizzBuzz.upto(3) == [1, 2, "Fizz"]
+  end
+
+  test 'upto/1 outputs "Buzz" for multiples of five' do
+    assert FizzBuzz.upto(5) == [1, 2, "Fizz", 4, "Buzz"]
+  end
+
+  test 'upto/1 outputs "FizzBuzz" for multiples of three and five' do
+    assert FizzBuzz.upto(15) == [
+      1, 2, "Fizz",
+      4, "Buzz", "Fizz",
+      7, 8, "Fizz",
+      "Buzz", 11, "Fizz",
+      13, 14, "FizzBuzz"
+    ]
+  end
+
+  test 'upto/1 only handles positive integers' do
+    assert_raise FunctionClauseError, fn ->
+      FizzBuzz.upto(0)
+    end
+  end
+end
