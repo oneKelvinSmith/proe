b866360f8334a27fa350cca10db9b640e7298423
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-06-02 23:11:47 +0100

    FizzBuzz with pattern matching

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,21 +1,10 @@
 defmodule FizzBuzz do
-  def upto(n) when n > 0 do
-    1..n |> Enum.map(&fizzbuzz/1)
-  end
+  def upto(n) when n > 0, do: 1..n |> Enum.map(&do_fizzbuzz/1)
 
-  defp fizzbuzz(n) do
-    cond do
-      rem(n, 3) == 0 and rem(n, 5) == 0 ->
-        "FizzBuzz"
+  defp do_fizzbuzz(n), do: do_fizzword(n, rem(n, 3), rem(n, 5))
 
-      rem(n, 3) == 0 ->
-        "Fizz"
-
-      rem(n, 5) == 0 ->
-        "Buzz"
-
-      true ->
-        n
-    end
-  end
+  defp do_fizzword(_n, 0, 0), do: "FizzBuzz"
+  defp do_fizzword(_n, 0, _), do: "Fizz"
+  defp do_fizzword(_n, _, 0), do: "Buzz"
+  defp do_fizzword(n, _, _), do: n
 end
