57916dc807bea23b9771324a1f84a233ce1ca52b
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-06-02 23:07:07 +0100

    FizzBuzz with transformation via Enum

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,18 +1,21 @@
 defmodule FizzBuzz do
-  def upto(n) when n > 0, do: do_downto(n, [])
-  defp do_downto(0, result), do: result
-  defp do_downto(current, result) do
-    next_answer =
-      cond do
-        rem(current, 3) == 0 and rem(current, 5) == 0->
-          "FizzBuzz"
-        rem(current, 3) == 0 ->
-          "Fizz"
-        rem(current, 5) == 0 ->
-          "Buzz"
-        true ->
-          current
-      end
-    do_downto(current - 1, [next_answer | result])
+  def upto(n) when n > 0 do
+    1..n |> Enum.map(&fizzbuzz/1)
+  end
+
+  defp fizzbuzz(n) do
+    cond do
+      rem(n, 3) == 0 and rem(n, 5) == 0 ->
+        "FizzBuzz"
+
+      rem(n, 3) == 0 ->
+        "Fizz"
+
+      rem(n, 5) == 0 ->
+        "Buzz"
+
+      true ->
+        n
+    end
   end
 end
