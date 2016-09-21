0e0b74afd0b9fa11787762ce0a8c9f5536d1d9fd
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-09-21 09:25:41 +0100

    Add example for linked process trapping exit

--- concurrency/lib/spawn/link.ex
+++ concurrency/lib/spawn/link.ex
@@ -9,7 +9,7 @@ defmodule Link do
   def run_without_link do
     spawn(Link, :sad_function, [])
     receive do
-      message ->
+      {:EXIT, _pid, message} ->
         IO.puts "MESSAGE RECEIVED: #{inspect message}"
     after 1000 ->
         IO.puts "Nothing happened as far as I am concerned"
@@ -19,7 +19,18 @@ defmodule Link do
   def run_with_link do
     spawn_link(Link, :sad_function, [])
     receive do
-      message ->
+      {:EXIT, _pid, message} ->
+        IO.puts "MESSAGE RECEIVED: #{inspect message}"
+    after 1000 ->
+        IO.puts "Nothing happened as far as I am concerned"
+    end
+  end
+
+  def run_with_trap do
+    Process.flag(:trap_exit, true)
+    spawn_link(Link, :sad_function, [])
+    receive do
+      {:EXIT, _pid, message} ->
         IO.puts "MESSAGE RECEIVED: #{inspect message}"
     after 1000 ->
         IO.puts "Nothing happened as far as I am concerned"
--- concurrency/test/spawn/link_test.exs
+++ concurrency/test/spawn/link_test.exs
@@ -6,20 +6,28 @@ defmodule LinkTest do
   test "noone is notified when a process dies" do
     assert capture_io(fn ->
       Link.run_without_link
-    end) == "Nothing happened as far as I am concerned\n"
+    end) == """
+    Nothing happened as far as I am concerned
+    """
   end
 
   test "a linked process dies as well" do
     trap = Process.flag(:trap_exit, true)
 
-    message = capture_io(fn ->
+    assert capture_io(fn ->
       Link.run_with_link
-    end)
-
-    assert message =~ "MESSAGE RECEIVED"
-    assert message =~ ":EXIT"
-    assert message =~ ":boom"
+    end) == """
+    MESSAGE RECEIVED: :boom
+    """
 
     Process.flag(:trap_exit, trap)
+  end
+
+  test "trapping the exit does not kill a linked process" do
+    assert capture_io(fn ->
+      Link.run_with_trap
+    end) == """
+    MESSAGE RECEIVED: :boom
+    """
   end
 end
